21b22a3a748192da6bbb6ed6a12d7bb039e249ee
softeng2021 <naeemgibson@gmail.com>
2021-07-31 17:42:59 -0400

    codeanddesign

---
+++ indx.js
@@ -0,0 +1,93 @@
+    var otTitle = document.getElementById("textBox"),
+        letters = document.getElementsByClassName('text')
+		sSpan = letters.length,
+		buttons = document.getElementsByClassName('button1'),
+        bButton = buttons.length,
+
+		whichLetter = 0,
+		trailAmount1 = 7,
+        whichButton = 0,
+        trailAmount2 = 5,
+        one = document.querySelector('#one'),
+        two = document.querySelector('#two'),
+        three = document.querySelector('#three'),
+        four = document.querySelector('#four'),
+        play = document.querySelector('.button')
+      
+        
+
+
+//Used to get random colors
+function getRandomColor() {
+	var letters = '0123456789ABCDEF';
+	var color = '#';
+	for (var i = 0; i < 6; i++) {
+		color += letters[Math.floor(Math.random() * 16)];
+	}
+	return color;
+}
+
+// //Animate color change 
+// setInterval(function() {
+// 	var rColor = getRandomColor();
+// 	var tColor = getRandomColor();
+// 	if (whichLetter < sSpan + trailAmount1) {
+// 		if (whichLetter > (trailAmount1 - 1)) {
+// 			letters[(whichLetter - trailAmount1)].style.color = 'whitesmoke';
+// 		}
+// 		if (whichLetter < sSpan) {
+// 			// letters[whichLetter].style.color = rColor;
+// 			letters[whichLetter].style.textShadow = '0px 0px 10px ' + tColor;
+// 		}
+// 		whichLetter++;
+// 	} else if (whichLetter > sSpan + (trailAmount1 - 1)) whichLetter = 0;
+// }, 75)
+
+setInterval(function() {
+	var rColor = getRandomColor();
+	var tColor = getRandomColor();
+	if (whichButton < bButton + trailAmount2) {
+		if (whichButton > (trailAmount2 - 1)) {
+			buttons[(whichButton - trailAmount2)].style.color = 'whitesmoke';
+		}
+		if (whichButton < bButton) {
+			buttons[whichButton].style.background = rColor;
+			buttons[whichButton].style.boxShadow = '0px 0px 10px ' + tColor;
+		}
+		whichButton++;
+	} else if (whichButton > bButton + (trailAmount2 - 1)) whichButton = 0;
+}, 75)
+
+one.addEventListener('click',(e) =>{
+    console.log('clicked1')
+    one.style.background = 'white';
+    const audio = this.document.querySelector('#first')
+    audio.play();
+})
+// one.addEventListener('mouseover',(e) =>{
+//     one.style.background = 'white';
+//     clearInterval()
+// })
+two.addEventListener('click',(e) =>{
+    console.log('clicked2')
+    two.style.background = 'white';
+    const audio = this.document.querySelector('#second')
+    audio.play();
+})
+three.addEventListener('click',(e) =>{
+    console.log('clicked3')
+    three.style.background = 'white';
+    const audio = this.document.querySelector('#third')
+    audio.play();
+})
+four.addEventListener('click',(e) =>{
+    console.log('clicked4')
+    four.style.background = 'white';
+    const audio = this.document.querySelector('#fourth')
+    audio.play();
+})
+play.addEventListener('click',(e) =>{
+    const audio = this.document.querySelector('#play')
+    audio.play(); 
+    console.log('play')
+})
--- script.js
+++ script.js
@@ -4,11 +4,14 @@ console.log('works');
 let win;
 let playerOn = false;
 let squares = document.querySelectorAll('.cell')
-let computerOn;
+let computerOn = true;
 let turn;
 let positions = [];
 let light = 0;
 let sound = true;
+let gameOn = false;
+let playerPositions = [];
+let correct = false;
 
 const topLeft = document.querySelector('#top-left')
 const topRight = document.querySelector('#top-right')
@@ -16,6 +19,12 @@ const bottomLeft = document.querySelector('#bottom-left')
 const bottomRight = document.querySelector('#bottom-right')
 const powerButton = document.querySelector('#power1')
 const startButton = document.querySelector('#start1')
+
+//Get Random color
+const r = Math.floor(Math.random() * 255);
+const g = Math.floor(Math.random() * 255);
+const b = Math.floor(Math.random() * 255);
+const newColor = `rgb(${r}, ${g}, ${b})`;
 
 function resetColor(){
     topLeft.style.opacity = 1;
@@ -27,120 +36,142 @@ function resetColor(){
     bottomLeft.style.opacity = 1;
     bottomLeft.style.background= "blue";
 }
+function flashLight(){
+    topLeft.style.background = 'white';
+    topRight.style.background = 'white';
+    bottomRight.style.background = 'white';
+    bottomLeft.style.background= 'white';
+}
 
+ function winGame() {
+     flashLight()
+     turnCounter.innerHTML = "winner!";
+     gameOn = false;
+     win = true;
+     }   
 powerButton.addEventListener('click',(e) =>{
-    if (powerButton.checked == true){
-        on = true;
+    if (powerButton.checked === true && startButton.checked === false){
+        gameOn = true;
        console.log("on")
-//        //Top Left Light
-//         function topLeftLightUp() {
-//   topLeft.style.opacity = 0.5;
-//   setTimeout (topLeftLightOff, 2000)}
-// topLeftLightUp()
-// function topLeftLightOff()
-// {topLeft.style.opacity = 1;}
+       //Top Left Light
+        function topLeftLightUp() {
+  topLeft.style.opacity = 0.5;
+  setTimeout (topLeftLightOff, 2000)}
+topLeftLightUp()
+function topLeftLightOff()
+{topLeft.style.opacity = 1;}
 
-// //Top Right Light
-// function topRightLightUp() {
-//     topRight.style.opacity = 0.5;}
-//     topRightLightUp();
-// setTimeout(topRightLightOff, 2000)
-// function topRightLightOff()
-// {topRight.style.opacity = 1;}
-
-// //Bottom Left Light
-// function bottomLeftLightUp() {
-//     bottomLeft.style.opacity = 0.5;
-// setTimeout(bottomLeftLightOff, 2000)}
-//     bottomLeftLightUp();
-// function bottomLeftLightOff()
-//     {bottomLeft.style.opacity = 1;}
-
-//     //Bottom Right
-// function bottomRightLightUp() {
-//     bottomRight.style.opacity = 0.5;
-// setTimeout(bottomRightLightOff, 2000)}
-//     bottomRightLightUp();
-// function bottomRightLightOff()
-//     {bottomRight.style.opacity = 1;}
-
-
-
-    powerLightsIntervalId = setInterval ( powerLights, 2075 );
-
-
-    function powerLights ()
-    {
-     //Top Left Light
-     function topLeftLightUp() {
-      topLeft.style.opacity = 0.5;
-const r = Math.floor(Math.random() * 255);
-const g = Math.floor(Math.random() * 255);
-const b = Math.floor(Math.random() * 255);
-const newColor = `rgb(${r}, ${g}, ${b})`;
-topLeft.style.backgroundColor = newColor;
-      setTimeout (topLeftLightOff, 500)}
-    topLeftLightUp()
-    function topLeftLightOff()
-    {topLeft.style.opacity = 1;}
-    
-    //Top Right Light
-    function topRightLightUp() {
-        topRight.style.opacity = 0.5;
-    const r = Math.floor(Math.random() * 255);
-    const g = Math.floor(Math.random() * 255);
-    const b = Math.floor(Math.random() * 255);
-    const newColor = `rgb(${r}, ${g}, ${b})`;
-    topRight.style.backgroundColor = newColor;  
-    setTimeout(topRightLightOff, 1000)}
+//Top Right Light
+function topRightLightUp() {
+    topRight.style.opacity = 0.5;}
     topRightLightUp();
-    function topRightLightOff()
-    {topRight.style.opacity = 1;}
-    
+setTimeout(topRightLightOff, 2000)
+function topRightLightOff()
+{topRight.style.opacity = 1;}
+
+//Bottom Left Light
+function bottomLeftLightUp() {
+    bottomLeft.style.opacity = 0.5;
+setTimeout(bottomLeftLightOff, 2000)}
+    bottomLeftLightUp();
+function bottomLeftLightOff()
+    {bottomLeft.style.opacity = 1;}
+
     //Bottom Right
-    function bottomRightLightUp() {
-        bottomRight.style.opacity = 0.5;
-const r = Math.floor(Math.random() * 255);
-const g = Math.floor(Math.random() * 255);
-const b = Math.floor(Math.random() * 255);
-const newColor = `rgb(${r}, ${g}, ${b})`;
-bottomRight.style.backgroundColor = newColor;
-    setTimeout(bottomRightLightOff, 1500)}
+function bottomRightLightUp() {
+    bottomRight.style.opacity = 0.5;
+setTimeout(bottomRightLightOff, 2000)}
     bottomRightLightUp();
-    function bottomRightLightOff()
+function bottomRightLightOff()
     {bottomRight.style.opacity = 1;}
 
-     //Bottom Left Light
-     function bottomLeftLightUp() {
-        bottomLeft.style.opacity = 0.5;
-const r = Math.floor(Math.random() * 255);
-const g = Math.floor(Math.random() * 255);
-const b = Math.floor(Math.random() * 255);
-const newColor = `rgb(${r}, ${g}, ${b})`;
-bottomLeft.style.backgroundColor = newColor;
-    setTimeout(bottomLeftLightOff, 2000)}
-    bottomLeftLightUp();
-    function bottomLeftLightOff()
-    {bottomLeft.style.opacity = 1;
-    bottomLeft.style.background = blue;}
+
+
+//     powerLightsIntervalId = setInterval ( powerLights, 2075 );
+
+
+//     function powerLights ()
+//     {
+//      //Top Left Light
+//      function topLeftLightUp() {
+//       topLeft.style.opacity = 0.5;
+// const r = Math.floor(Math.random() * 255);
+// const g = Math.floor(Math.random() * 255);
+// const b = Math.floor(Math.random() * 255);
+// const newColor = `rgb(${r}, ${g}, ${b})`;
+// topLeft.style.backgroundColor = newColor;
+//       setTimeout (topLeftLightOff, 500)}
+//     topLeftLightUp()
+//     function topLeftLightOff()
+//     {topLeft.style.opacity = 1;}
+    
+//     //Top Right Light
+//     function topRightLightUp() {
+//         topRight.style.opacity = 0.5;
+//     const r = Math.floor(Math.random() * 255);
+//     const g = Math.floor(Math.random() * 255);
+//     const b = Math.floor(Math.random() * 255);
+//     const newColor = `rgb(${r}, ${g}, ${b})`;
+//     topRight.style.backgroundColor = newColor;  
+//     setTimeout(topRightLightOff, 1000)}
+//     topRightLightUp();
+//     function topRightLightOff()
+//     {topRight.style.opacity = 1;}
+    
+//     //Bottom Right
+//     function bottomRightLightUp() {
+//         bottomRight.style.opacity = 0.5;
+// const r = Math.floor(Math.random() * 255);
+// const g = Math.floor(Math.random() * 255);
+// const b = Math.floor(Math.random() * 255);
+// const newColor = `rgb(${r}, ${g}, ${b})`;
+// bottomRight.style.backgroundColor = newColor;
+//     setTimeout(bottomRightLightOff, 1500)}
+//     bottomRightLightUp();
+//     function bottomRightLightOff()
+//     {bottomRight.style.opacity = 1;}
+
+//      //Bottom Left Light
+//      function bottomLeftLightUp() {
+//         bottomLeft.style.opacity = 0.5;
+// const r = Math.floor(Math.random() * 255);
+// const g = Math.floor(Math.random() * 255);
+// const b = Math.floor(Math.random() * 255);
+// const newColor = `rgb(${r}, ${g}, ${b})`;
+// bottomLeft.style.backgroundColor = newColor;
+//     setTimeout(bottomLeftLightOff, 2000)}
+//     bottomLeftLightUp();
+//     function bottomLeftLightOff()
+//     {bottomLeft.style.opacity = 1;
+//     bottomLeft.style.background = 'blue';}
    
-    }
+//     }
   
     }
-if (powerButton.checked == false) {
-    clearInterval(powerLightsIntervalId)
+if (startButton.checked === true || powerButton.checked == false) {
+    stopRandom()
     resetColor()
+    console.log('hey')
 }
+function stopRandom (){
+    clearInterval(powerLightsIntervalId)
+} 
 
 })
 
 startButton.addEventListener('click', (e) => {
-    if (playerOn || computerOn || win) {
-        
+    if (startButton.checked == true && powerButton.checked == true && playerOn || computerOn || win) {
+        gamePlay();
+        console.log('clicked')
+    }
+    if (powerButton.checked == false || startButton.checked == false) {
+        resetColor()
+        clearInterval(intervalID)
     }
 })
 
 function gamePlay() {
+    if (startButton.checked == true) {
     positions = [];
     light = 0
     intervalID = 0
@@ -148,13 +179,14 @@ function gamePlay() {
         positions.push(Math.floor(Math.random()*4)+1);
     } console.log(positions)
         computerOn = true;
-        intervalID = setInterval(gameTurn, 800)
+        intervalID = setInterval(gameTurn, 800)}
     }
-    gamePlay()
 
     function gameTurn() {
         playerOn = false;
-
+if (powerButton.checked == false || startButton.checked == false) {
+    return;
+}
         // Defining the players turn
         if (light == turn) {
             // Reset the game
@@ -165,58 +197,140 @@ function gamePlay() {
             playerOn = true
         }
 
-        if (computerOn){
+        if (computerOn == true){
+            computerOn == true;
             resetColor()
             // Here we want to mimic the power button code block 
             // but have it in sync with the random positions array
             setTimeout(() => {
                 if(positions[light] == 1)tpLft();
                 if(positions[light] == 2)tpRght();
-                if(positions[light] == 1)btmRght();
-                if(positions[light] == 1)btmLft();
+                if(positions[light] == 3)btmRght();
+                if(positions[light] == 4)btmLft();
                 light++
             }, 200);
         }
     }
 
 function tpLft(){
-    if (sound) {
+    if (sound == true) {
         let audio = document.getElementById('topleft')
         audio.play();}
         sound = true;
-        topLeft.style.opacity = 0.5;
+        topLeft.style.backgroundColor = 'whitesmoke';
 }
 
 function tpRght(){
-    if (sound) {
+    if (sound == true) {
         let audio = document.getElementById('topright')
         audio.play();}
         sound = true;
-        topRight.style.opacity = 0.5;
+        topRight.style.backgroundColor = 'whitesmoke';
 }
 
 function btmRght(){
-    if (sound) {
+    if (sound == true) {
         let audio = document.getElementById('btmright')
         audio.play();}
         sound = true;
-        bottomRight.style.opacity = 0.5;
+        bottomRight.style.backgroundColor = 'whitesmoke';
 }
 
 function btmLft(){
-    if (sound) {
+    if (sound == true) {
         let audio = document.getElementById('btmleft')
         audio.play();}
         sound = true;
-        topLeft.style.opacity = 0.5;
+        bottomLeft.style.backgroundColor = 'whitesmoke';
 }
 
+topLeft.addEventListener('click', (event) => {
+    if (gameOn == true && computerOn == false) {
+        playerPositions.push(1);
+        // check();
+        tpLft();
+        if(!win){
+            setTimeout(() => {
+               resetColor(); 
+            }, 300);
+        }
+    }
+})
+topRight.addEventListener('click', (event) => {
+    if (gameOn == true && computerOn == false) {
+        playerPositions.push(2);
+        // check();
+        tpRght();
+        if(!win){
+            setTimeout(() => {
+               resetColor(); 
+            }, 300);
+        }
+    }
+})
+bottomRight.addEventListener('click', (event) => {
+    if (gameOn == true && computerOn == false) {
+        playerPositions.push(3);
+        // check();
+        btmRght();
+        if(!win){
+            setTimeout(() => {
+               resetColor(); 
+            }, 300);
+        }
+    }
+})
+bottomLeft.addEventListener('click', (event) => {
+    if (gameOn == true && computerOn == false) {
+        playerPositions.push(4);
+        // check();
+        btmLft();
+        if(!win){
+            setTimeout(() => {
+               resetColor(); 
+            }, 300);
+        }
+    }
+})
 
 
+function check() {
+    if (playerOrder[playerOrder.length -1] !== positions[playerOrder.length-1]) {
+        correct = false;
+}
+    if (playerOrder.length == 16 && correct == true) {
+        winner ();
+    }
 
+    if (correct == false) {
+        flashLight();
+        turnCounter.innerHTML = 'Nope!'
+        setTimeout(() => {
+          turnCounter.innerHTML = turn;
+          resetColor()
 
+        if (pro == true){
+            gamePlay()
+        }else {
+            computerOn = true;
+            flash = 0;
+            playerPositions = [];
+            correct = true;
+            intervalID = setInterval(gameTurn, 800) 
+        }
+        }, 800);
+        sound == false;
 
-
+        if (turn == playerPositions.length && correct && win){
+            turn++
+            playerPositions=[]
+            computerOn = true
+            flash = 0
+            turnCounter.innerHTML = turn;
+            setInterval()(gameTurn,800)
+        }
+    }
+}
 
 
 
